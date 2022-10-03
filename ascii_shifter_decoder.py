591f479c3b72ab2c17e531b21f8f87446f643ba2
alb3rtov <albertovmrm@gmail.com>
2022-10-03 13:46:59 +0200

    Shift always negative and pass encoded textfile as argument

--- ascii_shifter_decoder.py
+++ ascii_shifter_decoder.py
@@ -1,33 +1,45 @@
 #!/usr/bin/python3
 
 import os.path
+import sys
 
 ''' Read file of encrypted text '''
 def read_file(path_file):
     file = open(path_file, mode='r')
     text = file.read()
     file.close()
+    
     bytes_object = bytes.fromhex(text)
     string = bytes_object.decode()
+    
     return string
 
 ''' Function converts plaintext to ciphertext using key '''
 def ascii_shift(key, text):
     result = ""
+    
     for letter in text:
-        ascii = ( ord(letter) + key - 32 ) % 94 + 32
+        ascii = ( ord(letter) - key - 32 ) % 94 + 32
         result = result + chr(ascii)
     return result
 
-path_file = input("Enter encrypted text file path: ")
+''' Main function '''
+def main():
+    if (len(sys.argv) == 2):
+        if (os.path.exists(sys.argv[1])):
+            shift = input("Enter shift: ")
+            key = int(shift)
 
-if (os.path.exists(path_file)):
-    shift = input("Enter shift: ")
-    key = int(shift)
+            text = read_file(sys.argv[1])
+            result = ascii_shift(key, text)
+            print("Result: ", result)
+            print()
+        else:
+            print(path_file + " file not found.")
+    else:
+        print()
+        print("Usage: ascii_shifter_decoder.py <encoded_text_file>");
+        print()
 
-    text = read_file(path_file)
-    result = ascii_shift(key, text)
-    print("Result: ", result)
-    print()
-else:
-    print(path_file + " file not found.")
+if __name__ == "__main__":
+    main()
