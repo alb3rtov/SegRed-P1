100f8c50c9a311408ef4088d76f42f530d2957b2
alb3rtov <albertovmrm@gmail.com>
2022-10-04 19:00:24 +0200

    Generate file with decoded data

--- ascii_shifter_decoder.py
+++ ascii_shifter_decoder.py
@@ -21,25 +21,35 @@ def read_packets(path_file):
                         string = string + chr(c)
                 list_strings.append(string)
 
-    return list_strings
+    return list_strings, len(packets)
 
 ''' Function converts plaintext to ciphertext using key '''
-def ascii_shift(key, text):
+def ascii_shift(key, text, filename, num_packets):
+    decoded_file_name = filename + "-decoded.txt" 
     
     for strings in text:
         result = ""
         for letter in strings:
             ascii = ( ord(letter) - key - 32 ) % 94 + 32
             result = result + chr(ascii)
-        print(result)
+        f = open(decoded_file_name, "a")
+        f.write(result + '\n\n')
+        f.close()
+    
+    print()
+    if os.path.exists(decoded_file_name):
+        print("Finished. Generated " + decoded_file_name + " file. " + num_packets + " packets have been read.")
+    else:
+        print("An error ocurred during generating decoded file.")
+    print()
 
 ''' Main function '''
 def main():
     if (len(sys.argv) == 3):
         if os.path.exists(sys.argv[1]) and sys.argv[2].isdigit():
             key = int(sys.argv[2])
-            text = read_packets(sys.argv[1]);
-            ascii_shift(key, text)
+            text, num_packets = read_packets(sys.argv[1]);
+            ascii_shift(key, text, sys.argv[1].split('.')[0], str(num_packets))
         else:
             print(sys.argv[1] + " file not found or shift number incorrect")
     else:
